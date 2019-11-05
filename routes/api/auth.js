1f3c70d248fb9aa7a62db24b022ce4a5f3bc72ab
ANMOL <43845658+Anmol270900@users.noreply.github.com>
2019-11-05 13:42:19 +0530

    profile model added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -49,6 +49,12 @@ router.post('/', [
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
+        
+        if(!isMatch) {
+            return res
+                .status(400)
+                .json({ errors: [{ msg: 'Invalid Credentials' }]});
+        }
 
         // Return jsonwebtoken
         const payload = {
